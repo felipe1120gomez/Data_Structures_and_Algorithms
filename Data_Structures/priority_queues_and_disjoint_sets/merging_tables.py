58dfdd3e2422dc2ff05065eef8db572c7133c252
felipe1120gomez <felipe92agudelo@gmail.com>
2021-05-17 20:30:19 -0500

    dictionaries changed by lists in merging_tables.py

--- Data_Structures/priority_queues_and_disjoint_sets/merging_tables.py
+++ Data_Structures/priority_queues_and_disjoint_sets/merging_tables.py
@@ -12,12 +12,12 @@ class Database:
         Number of rows in all tables
     max_row_count : int
         Maximum number of rows
-    ranks : dict()
+    ranks : list()
         Rank of each table, in the parent table it is equal to the height of the tree.
-    rows : dict()
+    rows : list()
         Rows on each table.
-    parents : dic()
-        Parent of each table
+    parents : list()
+        Parent of each table (sets)
 
     Methods
     -------
@@ -38,21 +38,20 @@ class Database:
                 Number of rows in all tables
             max_row_count : int
                 Maximum number of rows
-            ranks : dict()
+            ranks : list()
                 Rank of each table, in the parent table it is equal to the height of the tree.
-            rows : dict()
+            rows : list()
                 Rows on each table.
-            parents : dic()
-                Parent of each table
+            parents : list()
+                Parent of each table (sets)
         """
 
         self.row_counts = row_counts
         self.max_row_count = max(row_counts)
         n_tables = len(row_counts)
-        # Dictionaries to find tables in O(1).
-        self.ranks = {i: 1 for i in range(len(self.row_counts))} # Tables and ranks
-        self.rows = {i: self.row_counts[i] for i in range(len(self.row_counts))} # Tables and rows
-        self.parents = {i: i for i in range(n_tables)} # Tables and parents
+        self.ranks = [1 for i in range(len(self.row_counts))]
+        self.rows = [self.row_counts[i] for i in range(len(self.row_counts))]
+        self.parents = [i for i in range(n_tables)]
 
     def merge(self, dst, src):
         '''Merge two tables with union by rank heuristic and path comprehension.'''
@@ -70,29 +69,29 @@ class Database:
             self.rows[src_parent] = 0 # Remove rows from source table
             self.ranks[dst_parent] += self.ranks[src_parent] # Increase the rank of the destination table
             self.ranks[src_parent] = 0 # Rank of the source table at 0
-            self.parents[src_parent] = dst_parent # compress path
+            self.parents[src_parent] = dst_parent # Compress path
             if self.rows[dst_parent] > self.max_row_count:
-                self.max_row_count = self.rows[dst_parent] # update max_row
+                self.max_row_count = self.rows[dst_parent] # Update max_row
 
         else:
             self.rows[src_parent] += self.rows[dst_parent] # Copy rows from one table to another
             self.rows[dst_parent] = 0 # Remove rows from destination table
             self.ranks[src_parent] += self.ranks[dst_parent] # Increase the rank of the source table
             self.ranks[dst_parent] = 0 # Rank of the destination table at 0
-            self.parents[dst_parent] = src_parent # compress path
+            self.parents[dst_parent] = src_parent # Compress path
             if self.rows[src_parent] > self.max_row_count:
-                self.max_row_count = self.rows[src_parent] # update max_row
+                self.max_row_count = self.rows[src_parent] # Update max_row
 
         return True
 
     def get_parent(self, table):
         '''Returns the true parent of the table.'''
 
-        parent = self.parents.get(table)
+        parent = self.parents[table]
         # keep going until find the true father
         while table != parent:
             table = parent
-            parent = self.parents.get(table)
+            parent = self.parents[table]
         return parent # true parent
 
 
